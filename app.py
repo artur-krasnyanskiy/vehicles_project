37ec30564cbffad8e73df5e97f882e868df2dc74
artur-krasnyanskiy <artur-krasnyanskiy@users.noreply.github.com>
2022-12-18 07:51:36 -0800

    adjusted na values again

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-df = pd.read_csv('vehicles_us.csv', na_values=[])
+df = pd.read_csv('vehicles_us.csv')
 df['manufacturer'] = df['model'].apply(lambda x: x.split()[0])
 
 # create a text header above the dataframe
